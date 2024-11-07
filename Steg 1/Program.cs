afd7011fe5110dd2d2f0cd9f7272c1e4de676800
Theo Lee <theo.leehao@elev.ga.ntig.se>
2024-11-07 10:25:55 +0100

    2

--- Steg 1/Program.cs
+++ Steg 1/Program.cs
@@ -1,4 +1,17 @@
-﻿if (6 >= 3)
+﻿using System.Reflection.Metadata;
+
+// if (6 >= 3)
+// {
+//     System.Console.WriteLine("Hello World");
+// }
+
+System.Console.WriteLine("Enter Username");
+String User = Console.ReadLine();
+
+if (User == "noname")
 {
-    System.Console.WriteLine("Hello World");
+    System.Console.WriteLine("Welcome");
 }
+
+
+Console.ReadLine();
